84dffe2f2279aec7d02ff74f55dbbeb01a008e02
Aayush Gupta <guptaaayush432@gmail.com>
2023-02-01 23:11:11 +0530

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,12 @@
-import '@/styles/globals.css'
+import "../styles/globals.css";
+// require("dotenv").config();
+import { IcyProvider } from "@quicknode/icy-nft-hooks";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    // <IcyProvider apiKey={process.env.NFT_API_KEY}>
+    <IcyProvider apiKey={"c2efe28d35b34b79a544e51c16b303c1"}>
+      <Component {...pageProps} />
+    </IcyProvider>
+  );
 }
