def87e8d4752a4234f6d71ddc7fa6439c198d5e3
Aayush Gupta <guptaaayush432@gmail.com>
2023-02-03 21:13:11 +0530

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -5,7 +5,6 @@ import Layout from "components/Layout";
 export default function App({ Component, pageProps }) {
   return (
     <IcyProvider apiKey={process.env.QUICKNODE_NFT_API_KEY}>
-      {/* <IcyProvider apiKey={"c2efe28d35b34b79a544e51c16b303c1"}> */}
       <Layout>
         <Component {...pageProps} />
       </Layout>
